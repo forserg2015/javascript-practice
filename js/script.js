ae240246dfe66107ed261dd3d62fabfb662a99d8
Serhii Mozghovyi <forserg2015@gmail.com>
2022-06-30 00:56:27 -0700

    add console log

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//document.body.innerHTML = '<h1>Hello from JavaScript</h1>';
+/*JavaScript crash Course 
+at Bitwise ;*/
+console. log ('Serhii Mozghovvyi');
+console.log ('Food that was made by lovely hands');
